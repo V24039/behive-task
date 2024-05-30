11b18c7647b9e44b65bad5a3c06aa8718cb7db4b
Venu G Soganadgi <soganadgivenu@gmail.com>
2024-05-30 22:06:02 +0530

    initial commit

---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const Header = () => {
+  return (
+    <div className='w-full h-full'>
+      <Title/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Header/Title.tsx
@@ -0,0 +1,9 @@
+import background from '../assests/background.png';
+
+const Title = () => {
+  return (
+    <div className='w-full h-full border-2' style={{backgroundImage: `url(${background})`}}>Title</div>
+  )
+}
+
+export default Title
